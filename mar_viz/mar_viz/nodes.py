88769fe55a6086d6d7f73842cfaf238a9cedeba6
David Hunt <david.hunt@duke.edu>
2023-12-04 16:04:00 -0500

    added detections and tracking

--- mar_viz/mar_viz/nodes.py
+++ mar_viz/mar_viz/nodes.py
@@ -1,10 +1,12 @@
 import rclpy
 from avstack_bridge.objects import ObjectStateBridge
-from avstack_msgs.msg import ObjectState, ObjectStateArray
+from avstack_msgs.msg import ObjectStateArray,ObjectState
+from visualization_msgs.msg import Marker,MarkerArray
+from geometry_msgs.msg import Point, Pose, Quaternion, Vector3
+from vision_msgs.msg import BoundingBox3DArray,BoundingBox3D
+from std_msgs.msg import Header,ColorRGBA
+from avstack_msgs.msg import ObjectStateArray
 from rclpy.node import Node
-from std_msgs.msg import Header
-from vision_msgs.msg import BoundingBox3D, BoundingBox3DArray
-from visualization_msgs.msg import Marker, MarkerArray
 
 
 class AvstackBridgedVisualizer(Node):
@@ -19,20 +21,34 @@ class AvstackBridgedVisualizer(Node):
         self.gt_subscription = None
         self.gt_publisher = None
         self.gt_init_pub_sub()
-        # pub/sub for the ego detections
-        self.ego_subscription = self.create_subscription(
-            BoundingBox3DArray,
-            "ego/detections",
-            lambda msg: self.detections_sub_callback(msg, "ego"),
-            10,
-        )
 
-        self.ego_publisher = self.create_publisher(MarkerArray, "ego", 10)
+        #agent namespaces
+        self._agent_namespaces = []
 
+        #pub/sub for agent detections
+        self._detections_pubs = {}
+        self._detections_subs = {}
+
+        #pub/sub for agent tracks
+        self._tracks_pubs = {}
+        self._tracks_subs = {}
+
+        self.add_agent(agent_namespace="ego")
+
+        return
+
+    '''
+    ##########################################################
+    Ground Truth Initializations, Functions, and Callbacks
+    ##########################################################
+    '''
     def gt_init_pub_sub(self):
         # subscribe to ground truth object information
         self.gt_subscription = self.create_subscription(
-            ObjectStateArray, "object_truth", self.gt_sub_callback, 10
+            ObjectStateArray,
+            "object_truth",
+            self._gt_sub_callback,
+            10
         )
 
         # publish the visualized markers
@@ -40,19 +56,100 @@ class AvstackBridgedVisualizer(Node):
             MarkerArray, "object_truth/markers", 10
         )
 
-    def gt_sub_callback(self, msg: ObjectStateArray):
+    def _gt_sub_callback(self, msg:ObjectStateArray):
 
-        points = self.objectStateArray_to_markerArray(msg)
+        points = self._objectStateArray_to_markerArray(msg)
         self.gt_publisher.publish(points)
         # self.get_logger().info("Received {} objects".format(len(msg.states)))
 
-    def detections_sub_callback(self, msg: BoundingBox3DArray, agent: str):
+    '''
+    ##########################################################
+    Initialize new agent
+    ##########################################################
+    '''
+    def add_agent(self,agent_namespace:str):
 
-        markers = self.boundingBox3DArray_to_markerArray(msg)
-        self.ego_publisher.publish(markers)
-        # self.get_logger().info("Received {} detections from {}".format(len(msg.boxes),agent))
+        if agent_namespace not in self._agent_namespaces:
 
-    def boundingBox3DArray_to_markerArray(self, msg: BoundingBox3DArray) -> MarkerArray:
+            #add namespace to existing list of namespaces
+            self._agent_namespaces.append(agent_namespace)
+            self.get_logger().info("Visualizer added {}".format(agent_namespace))
+
+            #create a publisher/subscriber for the agent's detections
+            self._detections_init_pub_sub(agent_namespace)
+
+            #create a publisher/subscriber for the agent's tracks
+            self._tracks_init_pub_sub(agent_namespace)
+
+
+        else:
+            #TODO: Check this behavior with Spencer
+            self.get_logger().info("Visualizer attempted to add {}, but it already existed".format(agent_namespace))
+    
+    
+    '''
+    ##########################################################
+    Detections Initializations, Functions, and Callbacks
+    ##########################################################
+    '''
+    def _detections_init_pub_sub(self, agent_namespace:str):
+        
+        #initialize the subscriber
+        self._detections_subs[agent_namespace] = self.create_subscription(
+            BoundingBox3DArray,
+            "{}/detections".format(agent_namespace),
+            lambda msg: self._detections_sub_callback(msg,agent_namespace),
+            10
+        )
+
+        self._detections_pubs[agent_namespace] = self.create_publisher(
+            MarkerArray,
+            "{}/markers/detections".format(agent_namespace),
+            10
+        )
+
+    def _detections_sub_callback(self,msg:BoundingBox3DArray,agent_namespace:str):
+
+        markers = self._boundingBox3DArray_to_markerArray(msg)
+        self._detections_pubs[agent_namespace].publish(markers)
+        self.get_logger().info("Received {} detections from {}".format(len(msg.boxes),agent_namespace))
+
+    '''
+    ##########################################################
+    Track Initializations, Functions, and Callbacks
+    ##########################################################
+    '''
+
+    def _tracks_init_pub_sub(self, agent_namespace:str):
+        
+        #initialize the subscriber
+        self._tracks_subs[agent_namespace] = self.create_subscription(
+            ObjectStateArray,
+            "{}/tracks".format(agent_namespace),
+            lambda msg: self._tracks_sub_callback(msg,agent_namespace),
+            10
+        )
+
+        self._tracks_pubs[agent_namespace] = self.create_publisher(
+            MarkerArray,
+            "{}/markers/tracks".format(agent_namespace),
+            10
+        )
+
+    def _tracks_sub_callback(self,msg:ObjectStateArray,agent_namespace:str):
+
+        markers = self._objectStateArray_to_markerArray(msg)
+        self._tracks_pubs[agent_namespace].publish(markers)
+        self.get_logger().info("Received {} tracks from {}".format(len(msg.states),agent_namespace))
+
+    
+    '''
+    ##########################################################
+    Helper Fn's to convert to visualization messages
+    ##########################################################
+    '''
+
+    def _boundingBox3DArray_to_markerArray(self,msg:BoundingBox3DArray) -> MarkerArray:
 
         # define a new marker array
         detections = MarkerArray()
@@ -63,20 +160,21 @@ class AvstackBridgedVisualizer(Node):
         for i in range(len(msg.boxes)):
 
             detections.markers.append(
-                self.boundingBox3D_to_marker(
-                    bounding_box_3D=msg.boxes[i], header=header, id=i, name_space="ego"
+                self._boundingBox3D_to_marker(
+                    bounding_box_3D=msg.boxes[i],
+                    header=header,
+                    id = i,
+                    name_space="ego"
                 )
             )
 
         return detections
-
-    def boundingBox3D_to_marker(
-        self,
-        bounding_box_3D: BoundingBox3D,
-        header: Header,
-        id: int,
-        name_space: str = "ego",
-    ) -> Marker:
+    
+    def _boundingBox3D_to_marker(self,
+                            bounding_box_3D:BoundingBox3D,
+                            header:Header,
+                            id:int,
+                            name_space:str="ego") -> Marker:
 
         # define a new marker
         marker = Marker()
@@ -111,7 +209,9 @@ class AvstackBridgedVisualizer(Node):
 
     def objectStateArray_to_markerArray(self, msg: ObjectStateArray) -> MarkerArray:
 
-        # define a new marker array
+    def _objectStateArray_to_markerArray(self,msg:ObjectStateArray) -> MarkerArray:
+
+        #define a new marker array
         points = MarkerArray()
         points.markers = []
 
@@ -121,62 +221,25 @@ class AvstackBridgedVisualizer(Node):
         for i in range(len(msg.states)):
 
             # for each objectState in states, generate a new marker
-            if self.gt_visualization_method == "arrow":
-                points.markers.append(
-                    self.objectState_to_marker_arrows(
-                        object_state=msg.states[i], header=header, id=i, name_space="gt"
-                    )
+            points.markers.append(
+                self._objectState_to_marker(
+                    object_state=msg.states[i],
+                    header=header,
+                    id = i,
+                    name_space="gt"
                 )
-            elif self.gt_visualization_method == "bb":
-
-                points.markers.append(
-                    self.objectState_to_marker_bb(
-                        object_state=msg.states[i], header=header, id=i, name_space="gt"
-                    )
-                )
-
+            )
+        
         return points
 
-    def objectState_to_marker_arrows(
-        self, object_state: ObjectState, header: Header, id: int, name_space: str = "gt"
-    ) -> Marker:
 
-        # define a new marker
-        marker = Marker()
+    
 
-        # initialize the header
-        marker.header.frame_id = header.frame_id
-        marker.header.stamp = header.stamp
-
-        # define the marker attributes
-        marker.ns = name_space
-        marker.id = id
-        marker.type = Marker.ARROW
-
-        # define the scale
-        marker.scale.x = 5.0
-        marker.scale.y = 2.5
-        marker.scale.z = 3.0
-
-        # set the behavior to ADD for now
-        marker.action = Marker.ADD
-
-        # define the pose and point
-        marker.pose.orientation = object_state.attitude
-        marker.pose.orientation.w = -1.0 * marker.pose.orientation.w
-        marker.pose.position = object_state.position
-
-        # define the color
-        marker.color.a = 1.0
-        marker.color.r = 0.0
-        marker.color.g = 0.0
-        marker.color.b = 1.0
-
-        return marker
-
-    def objectState_to_marker_bb(
-        self, object_state: ObjectState, header: Header, id: int, name_space: str = "gt"
-    ) -> Marker:
+    def _objectState_to_marker(self,
+                            object_state:ObjectState,
+                            header:Header,
+                            id:int,
+                            name_space:str="gt") -> Marker:
 
         # define a new marker
         marker = Marker()
@@ -208,6 +271,12 @@ class AvstackBridgedVisualizer(Node):
         marker.color.b = 1.0
 
         return marker
+
+    '''
+    ##########################################################
+    Defining Custom Colors for Visualization
+    ##########################################################
+    '''
 
 
 def main(args=None):
