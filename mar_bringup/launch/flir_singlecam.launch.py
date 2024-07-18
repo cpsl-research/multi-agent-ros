94b36c35da6ad8248ba03b8f9687dabf8d6ee802
spencer@primus <spencer.hallyburton@duke.edu>
2024-07-18 13:45:30 -0400

    WIP on getting flir cameras running in mar

---
+++ mar_bringup/launch/flir_singlecam.launch.py
@@ -0,0 +1,57 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+
+from launch_ros.actions import SetRemap
+from launch_ros.substitutions import FindPackageShare
+
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription, GroupAction
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import PathJoinSubstitution
+
+
+def generate_launch_description():
+    flir_camera_parameters = PathJoinSubstitution([
+        FindPackageShare("mar_bringup"),
+        "config",
+        "cameras",
+        "north-building-four-cameras",
+        "camera-1",
+        "parameters.yaml",
+    ])
+
+    flir_camera_calibration = "file:///" + os.path.join(
+        get_package_share_directory("mar_bringup"),
+        "config",
+        "cameras",
+        "north-building-four-cameras",
+        "camera-1",
+        "calibration.yaml",
+    )
+
+    flir_include = GroupAction(
+        actions=[
+            # SetRemap(src="", dst=""),
+            IncludeLaunchDescription(
+                PythonLaunchDescriptionSource([
+                    PathJoinSubstitution([
+                        FindPackageShare('mar_drivers'),
+                        'launch',
+                        'flir_singlecam_driver_node.launch.py'
+                    ])
+                ]),
+                launch_arguments={
+                    'camera_type': 'blackfly_s',
+                    'serial': "'24172037'", # "'22395929'",  #"'24131919'",
+                    'gev_scps_packet_size': "9000",
+                    'parameter_file': flir_camera_parameters,
+                    'camerainfo_url': flir_camera_calibration,
+                }.items(),
+            )
+        ]
+    )
+
+    
+
+    return LaunchDescription([flir_include])
--- mar_viz/setup.py
+++ mar_viz/setup.py
@@ -4,7 +4,7 @@ package_name = 'mar_viz'
 
 setup(
     name=package_name,
-    version='0.0.0',
+    version='0.0.1',
     packages=find_packages(exclude=['test']),
     data_files=[
         ('share/ament_index/resource_index/packages',
@@ -15,12 +15,12 @@ setup(
     zip_safe=True,
     maintainer='spencer',
     maintainer_email='20426598+roshambo919@users.noreply.github.com',
-    description='TODO: Package description',
+    description='Visualization tools for multi agent applications',
     license='TODO: License declaration',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            "visualizer = mar_viz.nodes:main"
+            "rqt_viz = mar_viz.rqt:main"
         ],
     },
 )
