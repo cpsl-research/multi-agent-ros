3eb55c467f4e0db874b03dbcfd8123774868f6c6
Spencer Hallyburton <20426598+roshambo919@users.noreply.github.com>
2024-07-25 15:47:41 -0400

    Fix detection display bug

--- rosbag_playback/play_rosbag.sh
+++ rosbag_playback/play_rosbag.sh
@@ -5,7 +5,7 @@ set -e
 ros2 bag play \
     --loop \
     --delay=1 \
-    --rate=2 \
+    --rate=0.5 \
     --clock=100 \
     --qos-profile-overrides-path qos_override.yaml \
     '/data/shared/CARLA/rosbags/carla_dataset'
