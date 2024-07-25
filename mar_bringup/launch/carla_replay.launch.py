db9c28e8561f8aa1de911a641a0ede5a7f898a17
spencer@primus <spencer.hallyburton@duke.edu>
2024-07-25 12:20:46 -0400

    WIP on getting tracking running through

--- mar_bringup/launch/carla_replay.launch.py
+++ mar_bringup/launch/carla_replay.launch.py
@@ -1,40 +1,18 @@
-import os
-
-from ament_index_python.packages import get_package_share_directory
-from launch import LaunchDescription
-from launch.actions import Shutdown
-from launch.substitutions import LaunchConfiguration
-from launch_ros.actions import Node
+import launch
 
 
 def generate_launch_description():
-    sim_config = os.path.join(
-        get_package_share_directory("mar_bringup"),
-        "config",
-        "simulator",
-        "carla_point_simulator.yml",
-    )
-
-    output_folder = LaunchConfiguration("output_folder")
-
-    return LaunchDescription(
+    return launch.LaunchDescription(
         [
-            Node(
-                package="mar_carla_sim",
-                executable="carla_simulator",
-                name="simulator",
-                parameters=[
-                    sim_config,
-                    {"output_folder": output_folder},
+            launch.actions.ExecuteProcess(
+                cmd=[
+                    "ros2",
+                    "bag",
+                    "play",
+                    "-l",
+                    "/data/shared/CARLA/rosbags/carla_dataset",
                 ],
-                arguments=["--ros-args", "--log-level", "INFO"],
-                # remappings=[
-                #     *[
-                #         (f"/agent{i}/detections", remap)
-                #         for i, remap in enumerate(agent_remaps_configs)
-                #     ]
-                # ],
-                on_exit=Shutdown(),
-            ),
+                output="screen",
+            )
         ]
     )
